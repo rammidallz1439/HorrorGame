97d183ac968b642fbe9393bac202fbb8296ba859
davidjones12300 <116877519+davidjones12300@users.noreply.github.com>
2022-10-31 15:41:59 +0530

    UI Added

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Reload()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
