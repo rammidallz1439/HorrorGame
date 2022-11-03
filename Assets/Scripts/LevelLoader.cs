d8f80c48ee059be28887cf1077c0a19288b70550
davidjones12300 <116877519+davidjones12300@users.noreply.github.com>
2022-11-03 12:57:02 +0530

    ui

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button playButton;
+
+    public void Start()
     {
-        
+        playButton.onClick.AddListener(() => { StartGame(); });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
-    }
-    public void Reload()
-    {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("initialScene");
     }
     public void QuitGame()
     {
