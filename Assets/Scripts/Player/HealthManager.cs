ec81f78e071a7b9dc97ec081293452b15dc9efb7
Rameshreddy001 <75834162+rammidallz1439@users.noreply.github.com>
2022-11-11 15:36:28 +0530

    dfgdfg

--- Assets/Scripts/Player/HealthManager.cs
+++ Assets/Scripts/Player/HealthManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
 {
     public static HealthManager _HM;
+   
+    public static bool healthZero = false;
     public float _health;
     public Slider healthBar;
     public float reduceVelocity;
@@ -15,12 +17,39 @@ public class HealthManager : MonoBehaviour
         healthBar.value = 1f;
         _health = healthBar.value;
     }
+   
+        
+    
     private void Update()
     {
         healthBar.value = Mathf.Lerp(healthBar.value,_health,reduceVelocity);
+        if (healthBar.value > 0)
+        {
+            MainHealth._mg.lives = 5;
+            healthZero = false;
+            healthBar.value = Mathf.Lerp(healthBar.value, _health, reduceVelocity);
+        }
+        
+        if (healthBar.value <= 0)
+        {
+            healthZero = true;
+            MainHealth._mg.lives -= 1;
+            PlayerPrefs.SetInt("LivesCount", MainHealth._mg.lives);
+        }
+      
     }
     public void TakeDamage(float damage)
     {
         _health -= damage;
+        if (_health > 0)
+        {
+            _health -= damage;
+            if (_health <= 0.0f)
+            {
+                int lifes = PlayerPrefs.GetInt("LivesCount");
+                lifes -= 1;
+                PlayerPrefs.SetInt("LivesCount", lifes);
+            }
+        }
     }
 }
